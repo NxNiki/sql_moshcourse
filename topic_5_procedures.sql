18ddb8c2ea22ac03b968513b941a076de0a82efb
Xin Niu <michaelniki1988@gmail.com>
2022-10-23 22:01:24 -0400

    Add files via upload

--- topic_5_procedures.sql
+++ topic_5_procedures.sql
@@ -1,7 +1,7 @@
 #use sql_invoicing; 
 
 drop procedure if exists get_clients;
-delimiter $$
+delimiter $$ ## this avoid MySQL to excute codes saved in the procedure by setting the whole procedure definition as a single unit
 create procedure get_clients()
 begin
 	select * from clients;
@@ -25,8 +25,47 @@ begin
 		and p.payment_method = ifnull(payment_method, p.payment_method)
 	;
 end $$
+delimiter ;
 
-delimieter ;
+delimiter $$
+## use procedure to update data:
+create procedure make_payments (
+	invoice_id INT
+    , payment_amount DECIMAL(9, 2) # total number of digits, digits after the decimal point.
+    , payment_date DATE
+)
+begin
+	if payment_amount <= 0 then
+		signal sqlstate '22003'
+			set message_text = 'invalid payment amount';
+	end if;
+	update invoices i
+		set 
+			i.payment_total = payment_amount
+            , i.payment_date = payment_date
+		where i.invoice_id = invoice_id;
+end $$
+delimiter ;
 
+## output parameters:
+
+delimiter $$
+## variables:
+create procedure get_risk_factor ()
+begin
+	declare risk_factor decimal(9, 2) default 0;
+    declare invoices_total decimal(9, 2);
+    declare invoices_count int;
+    
+    select count(*), sum(invoice_total)
+    into invoices_count, invoices_total
+    from invoices;
+    
+    set risk_factor = invoices_total / invoices_count * 5;
+    
+    select risk_factor;
+end
+
+delimiter ;
 
 
