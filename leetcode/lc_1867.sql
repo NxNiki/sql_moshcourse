5b73a6e537c8e6c32c6db5240993a98acc9893e9
Xin Niu <michaelniki1988@gmail.com>
2025-03-13 06:04:32 -0700

    Update lc_1867.sql

--- leetcode/lc_1867.sql
+++ leetcode/lc_1867.sql
@@ -18,3 +18,20 @@ group by
     order_id
 having max(quantity) > (select max(avg_quantity) from average_quantity)
 
+
+solution 2:
+
+select 
+    order_id
+from (
+    select
+        order_id,
+        max(avg(quantity)) over() as max_avg_quantity,
+        max(quantity) as max_quantity
+    from
+        OrdersDetails
+    group by 
+        order_id
+) t
+where max_quantity > max_avg_quantity
+
