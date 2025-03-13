f6c3188daa010a55f3050698cc8ebba08d341517
Xin Niu <michaelniki1988@gmail.com>
2025-03-13 05:41:25 -0700

    Update lc_1321.sql

--- leetcode/lc_1321.sql
+++ leetcode/lc_1321.sql
@@ -22,3 +22,18 @@ group by
 having
     count(*) = 7
 
+
+-- solution 2:
+
+with cte as (
+    select
+        visited_on,
+        sum(amount) over (order by visited_on range between interval 6 day preceding  and current row) as amount,
+        min(visited_on) over() as 1st_date
+    from Customer
+)
+
+select distinct visited_on, amount, round(amount/7, 2) average_amount 
+from cte
+where visited_on >= 1st_date + 6
+
